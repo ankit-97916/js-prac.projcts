54e1d718f2e7128a11fa3f6b15268f721e1183d4
ankit <kumarankittdl31@gmail.com>
2024-12-08 18:37:56 +0530

    just format the code

--- To-do list app/app.js
+++ To-do list app/app.js
@@ -1,26 +1,26 @@
 const item = document.querySelector("#item");
 const toDoBox = document.querySelector("#to-do-box");
 
-item.addEventListener("keyup" , function(event) {
-    if(event.key == "Enter"){ // event key for perticular alphavet.
-        addTodo(this.value);// this.value for input text after enter, this = item.
-        this.value = ""; // for empty the input after enter
-
-    }
-})
+item.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    // event key for perticular alphavet.
+    addTodo(this.value); // this.value for input text after enter, this = item.
+    this.value = ""; // for empty the input after enter
+  }
+});
 
 const addTodo = (item) => {
-    const listitem = document.createElement("li"); // crete list by js
-    listitem.innerHTML = `${item}
-            <i>❎</i>`
+  const listitem = document.createElement("li"); // crete list by js
+  listitem.innerHTML = `${item}
+            <i>❎</i>`;
 
-            listitem.addEventListener("click", function() {
-                this.classList.toggle("done");
-            })
+  listitem.addEventListener("click", function () {
+    this.classList.toggle("done");
+  });
 
-            listitem.querySelector("i").addEventListener("click", function() {
-                listitem.remove();
-            })
+  listitem.querySelector("i").addEventListener("click", function () {
+    listitem.remove();
+  });
 
-            toDoBox.appendChild(listitem);
-}
+  toDoBox.appendChild(listitem);
+};
