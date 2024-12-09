c5643e016aca1512016e688750ccdf1fabb0c8ef
ankit <kumarankittdl31@gmail.com>
2024-12-09 11:06:27 +0530

    new practice project

---
+++ Color switcher/app.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
